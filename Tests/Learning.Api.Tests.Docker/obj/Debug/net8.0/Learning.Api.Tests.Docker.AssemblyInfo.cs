0d0ebebb3abc4099cf61d1fbdc2360a46142c05c
Amit Pandey <amit.p@icreon.com>
2025-01-21 17:33:26 +0530

    removed unnecessary files

--- Tests/Learning.Api.Tests.Docker/obj/Debug/net8.0/Learning.Api.Tests.Docker.AssemblyInfo.cs
+++ Tests/Learning.Api.Tests.Docker/obj/Debug/net8.0/Learning.Api.Tests.Docker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning.Api.Tests.Docker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a99b3f7f6b8073eac6157f88d48593875b9f9b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b450300b484b90c1fc92f826ebc409eee2eabb3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning.Api.Tests.Docker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning.Api.Tests.Docker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
